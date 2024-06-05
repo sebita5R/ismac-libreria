a9a64e0d8207f433e412dd121a5ae4baf807f42c
MATUTINO <MATUTINO@192.168.4.135>
2024-06-05 10:25:25 -0500

    asdasd

--- Ismac-libreria/src/com/distribuida/entities/Categoria.java
+++ Ismac-libreria/src/com/distribuida/entities/Categoria.java
@@ -8,7 +8,7 @@ public class Categoria {
 	private int idCategoria;
 	private String categoria;
 	private String descripcion;
-	//ñskjzaa
+	//ñskjzaaadasdasdasasdasdasdasd
 	public Categoria (){}
 	
 	public Categoria(int idCategoria, String categoria, String descripcion) {
